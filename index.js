afb0286eca723f1fcffe7b10453b78ab54887dcb
Luke <95511211+lcpenner@users.noreply.github.com>
2021-12-21 23:11:47 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,39 @@
+let num1 = 8
+let num2 = 2
+document.getElementById("num1-el").textContent = num1
+document.getElementById("num2-el").textContent = num2
+
+// Create four functions: add(), subtract(), divide(), multiply()
+// Call the correct function when the user clicks on one of the buttons
+// Perform the given calculation using num1 and num2
+// Render the result of the calculation in the paragraph with id="sum-el"
+
+// E.g. if the user clicks on the "Plus" button, you should render
+// "Sum: 10" (since 8 + 2 = 10) inside the paragraph with id="sum-el"
+
+let sumEl = document.getElementById("sum-el").textContent = "Solution: "
+
+
+function add ()
+{
+  let result = num1 + num2
+document.getElementById("sum-el").textContent = "Solution: "+ result
+}
+
+function subtract ()
+{
+let result = num1 - num2
+document.getElementById("sum-el").textContent = "Solution: "+ result
+}
+
+function divide()
+{
+let result = num1 / num2
+document.getElementById("sum-el").textContent = "Solution: "+ result
+}
+
+function multiply ()
+{
+let result = num1 * num2
+document.getElementById("sum-el").textContent = "Solution: "+ result
+}
